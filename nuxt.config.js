390de3d6575601062a9aca4eda4612d501384cf1
Cuke7 <cuke.dev@mail.com>
2021-12-23 16:17:07 +0100

    tset

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ["~/assets/style"],
+  css: ["~/assets/styles"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
